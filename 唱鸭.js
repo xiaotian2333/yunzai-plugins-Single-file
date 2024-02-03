a567032457c54efe682ddce0d891570d2853c4e8
xiaotian2333 <1719549416@qq.com>
2024-02-03 21:12:57 +0800

    唱鸭支持歌词发送

--- 唱鸭.js
+++ 唱鸭.js
@@ -11,7 +11,7 @@ export class example extends plugin {
 	constructor() {
 		super({
 			name: '唱鸭',
-			dsc: '小天开发',
+			dsc: '随机返回一段唱鸭的音频片段',
 			event: 'message',
 			priority: 5000,
 			rule: [{
@@ -23,6 +23,28 @@ export class example extends plugin {
 	}
 	
 	async changya(e) {
-		this.e.reply(segment.record('https://api.yujn.cn/api/changya.php?type=mp3'))
+		fetch('https://api.cenguigui.cn/api/singduck/')
+  			.then(response => {
+				if (!response.ok) {
+					logger.erro('网络请求失败');
+				}
+    		return response.json();
+  			})
+			.then(data => {
+				// 提取出data中的audioSrc并发送语音
+				logger.debug('[唱鸭]获取到歌曲链接：',data.data.audioSrc)
+				this.e.reply(segment.record(data.data.audioSrc))
+				// 提取出data中的lyrics并发送歌词
+				logger.debug('[唱鸭]获取到歌词：',data.data.lyrics)
+				// 处理歌词换行
+				let lyrics = data.data.lyrics.replace(/ /g, "\n");
+				lyrics = "歌手：" + data.data.nickname + "\n" + lyrics;
+				e.reply(lyrics)
+			})
+			.catch(error => {
+				//输出错误提示
+				logger.error('获取错误：', error);
+			});
 	}
 }
+
