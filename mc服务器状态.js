be249d8f511d270473c02c9af7e133c6b225797e
xiaotian2333 <1719549416@qq.com>
2024-07-06 21:51:47 +0800

    修复服务器未设置logo时报错，新增正版验证检测

--- mc服务器状态.js
+++ mc服务器状态.js
@@ -4,7 +4,7 @@
 import plugin from '../../lib/plugins/plugin.js'
 
 /**默认服务器，没有发送域名/ip时使用 */
-const Default_server = 'mc.hypixel.net'
+const Default_server = 'mc.xt-url.com'
 /**使用默认查询时的提示 */
 const Default_Tips = '没有发送服务器信息，查询默认服务器'
 
@@ -13,21 +13,20 @@ const Domain = /^(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z
 const ip = /\b(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b(?:\:\d{1,5})?/ //匹配ip，支持端口号
 
 export class example extends plugin {
-	constructor() {
-		super({
-			name: 'mc服务器状态',
-			dsc: '通过api获取mc-java服务器的状态',
-			event: 'message',
-			priority: 5000,
-			rule: [{
-					reg: '^#?(mc|MC|Mc|我的世界|java|jv|minecraft|Minecraft)(服务器|版)?(状态情况|状态|情况)',
-					fnc: 'java'
-				}
-			]
-		})
-	}
-	
-	async java(e) {
+  constructor() {
+    super({
+      name: 'mc服务器状态',
+      dsc: '通过api获取mc-java服务器的状态',
+      event: 'message',
+      priority: 5000,
+      rule: [{
+        reg: '^#?(mc|MC|Mc|我的世界|java|jv|minecraft|Minecraft)(服务器|版)?(状态情况|状态|情况)',
+        fnc: 'java'
+      }]
+    })
+  }
+
+  async java(e) {
     // 简化消息变量，同时方便调用
     let msg = e.msg
     // 删除不需要的部分
@@ -41,13 +40,13 @@ export class example extends plugin {
     // 使用test方法检查字符串是否符合正则表达式
     if (Domain.test(msg) || ip.test(msg)) {
       fetch(`https://api.mcstatus.io/v2/status/java/${msg}`)
-          .then(response => {
+        .then(response => {
           if (!response.ok) {
             logger.erro('网络请求失败')
             e.reply('网络请求失败')
           }
-          return response.json();
-          })
+          return response.json()
+        })
         .then(data => {
           // 开始解析服务器数据
           let msglist = `服务器地址：${msg}\n`
@@ -59,13 +58,32 @@ export class example extends plugin {
             return true
           }
 
+          // 正版验证状态
+          if (data.eula_blocked) {
+            msglist += `正版验证：开启\n`
+          } else if (!data.eula_blocked) {
+            msglist += `正版验证：关闭\n`
+          }
+          else {
+            msglist += `正版验证：无法判断，请查看日志输出\n`
+            logger.error(`正版验证值无法判断，接口返回：${data.eula_blocked}`)
+          }
+
           msglist += `版本：${data.version.name_clean}\n`
           msglist += `介绍：\n${data.motd.clean.replace(' ', '')}\n`
           msglist += `在线玩家数：${data.players.max}/${data.players.online}`
 
           // 服务器图片
-          const img = data.icon.replace("data:image/png;base64,", "base64://")
-          e.reply([segment.image(img),msglist])
+          const regex = /^data:image\/png;base64,/
+          if (regex.test(data.icon)) {
+            const img = data.icon.replace("data:image/png;base64,", "base64://")
+            e.reply([segment.image(img), msglist])
+          } else if (data.icon === null) {
+            e.reply(['[该服务器没有设置LOGO]\n', msglist])
+          } else {
+            e.reply(['[该服务器的LOGO无法识别]\n', msglist])
+          }
+
           return true
         })
         .catch(error => {
@@ -75,8 +93,8 @@ export class example extends plugin {
           return false
         })
     } else {
-        e.reply('请输入正确的域名或IP，支持带有端口号')
-        return false
+      e.reply('请输入正确的域名或IP，支持带有端口号')
+      return false
     }
-	}
+  }
 }
