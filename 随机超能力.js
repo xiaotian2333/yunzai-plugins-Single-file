cc2d4a1f9e23e384a4070bc33c087357ecb9c7c1
xiaotian2333 <1719549416@qq.com>
2024-08-26 15:24:20 +0800

    新增随机超能力

--- 随机超能力.js
+++ 随机超能力.js
@@ -1,52 +1,41 @@
 // 插件作者 xiaotian2333
 // 开源地址 https://github.com/xiaotian2333/yunzai-plugins-Single-file
 
-import fs from 'fs'
+// 欢迎投稿超能力列表，前往 https://github.com/xiaotian2333/yunzai-plugins-Single-file/discussions/3 即可投稿
 
-// 文件路径  
-const cnl_path = 'D:/资料/github-blog仓库/oss超能力列表.txt'
-const fzy_path = 'D:/资料/github-blog仓库/oss副作用列表.txt'
+// 能力列表链接
+const cnl_url = 'https://oss.xt-url.com/超能力列表.txt'
+const fzy_url = 'https://oss.xt-url.com/副作用列表.txt'
 
-const cnl_url = ''
-const fzy_url = ''
-
-// 读取超能力列表
-let cnl_list = []
-try {
-    cnl_list = fs.readFileSync(cnl_path, 'utf8')
-    cnl_list = cnl_list.split(/\r?\n/)
-
-    if (cnl_list.length > 0) {
-        logger.error('超能力列表文件为空')
-    }
-} catch (error) {
-    // 处理可能的错误，比如文件不存在或读取权限问题
-    logger.error('读取文件时发生错误:', error)
-}
+/**
+ * 获取能力列表，返回一个数组
+ * @param url 资源列表链接
+ */
+async function get_data(url) {
+    let result = await fetch(url, {
+      headers: {
+        'User-Agent': 'Special-ability(author by xiaotian2333) github(https://github.com/xiaotian2333/yunzai-plugins-Single-file)'
+      }
+    })
+    result = await result.text()
+    return result.split(/\r?\n/)
+  }
 
 // 读取超能力列表
-let fzy_list = []
-try {
-    fzy_list = fs.readFileSync(fzy_path, 'utf8')
-    fzy_list = fzy_list.split(/\r?\n/)
-
-    if (fzy_list.length > 0) {
-        logger.error('副作用列表文件为空')
-    }
-} catch (error) {
-    // 处理可能的错误，比如文件不存在或读取权限问题
-    logger.error('读取文件时发生错误:', error)
-}
+const cnl_list = await get_data(cnl_url)
+// 读取副作用列表
+const fzy_list = await get_data(fzy_url)
 
 import plugin from '../../lib/plugins/plugin.js'
+logger.info("随机超能力初始化完毕")
 
 export class nb extends plugin {
     constructor() {
         super({
-            name: '综合帮助',
-            dsc: '发送综合帮助图片',
+            name: '随机超能力',
+            dsc: '获取一个超能力及对应的副作用',
             event: 'message',
-            priority: -99999,
+            priority: 5000,
             rule: [
                 {
                     reg: "超能力",
@@ -60,7 +49,7 @@ export class nb extends plugin {
         // 随机选择一行  
         const cnl = cnl_list[Math.floor(Math.random() * cnl_list.length)]
         const fzy = fzy_list[Math.floor(Math.random() * fzy_list.length)]
-        e.reply(`你的超能力是：\n${cnl}\n但是：\n${fzy}`)
+        e.reply(`你的超能力是：\n${cnl}\n但是：\n${fzy}`, true)
         return true
     }
 }
