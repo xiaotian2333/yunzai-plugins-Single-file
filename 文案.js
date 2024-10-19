549529d099145982fbb8c8d089d1fb609f96bc50
xiaotian2333 <1719549416@qq.com>
2024-10-19 13:50:08 +0800

    新增古诗词，优化正则

--- 文案.js
+++ 文案.js
@@ -5,12 +5,12 @@
 * @param url 要请求的url
 */
 async function get_data(url) {
-    let result = await fetch(url, {
-        headers: {
-          'User-Agent': 'copywriting (author by xiaotian2333) github(https://github.com/xiaotian2333/yunzai-plugins-Single-file)'
-        }
-      })
-    return result
+  let result = await fetch(url, {
+    headers: {
+      'User-Agent': 'copywriting (author by xiaotian2333) github(https://github.com/xiaotian2333/yunzai-plugins-Single-file)'
+    }
+  })
+  return result
 }
 
 /** 统一图片请求
@@ -53,61 +53,72 @@ async function imgUrlToBase64(url) {
 
 
 export class copywriting extends plugin {
-    constructor() {
-        super({
-            name: '文案',
-            dsc: '搜集各类文案API',
-            event: 'message',
-            priority: 5000,
-            rule: [
-                {
-                    reg: '^#?(每|今)日一句.*$',
-                    fnc: 'dsapi'
-                },
-                {
-                    reg: "^#*(一言|壹言)$",
-                    fnc: "hitokoto",
-                },
-                {
-                    reg: '^#?(每日|今日|本日)?(新闻|60s|60S).*$',
-                    fnc: 'news'
-                },
-                {
-                  reg: '^#?(青年)?大学习',
-                  fnc: 'qndxx'
-              }
-            ]
-        })
-    }
+  constructor() {
+    super({
+      name: '文案',
+      dsc: '搜集各类文案API',
+      event: 'message',
+      priority: 5000,
+      rule: [
+        {
+          reg: '^#?(每|今)(日|天)(一)?(句|言|英语).*$',
+          fnc: 'dsapi'
+        },
+        {
+          reg: "^#*(一言|壹言)$",
+          fnc: "hitokoto",
+        },
+        {
+          reg: '^#?(每日|今日|本日)?(新闻|60s|60S).*$',
+          fnc: 'news'
+        },
+        {
+          reg: '^#?(青年)?大学习(完成|截图)',
+          fnc: 'qndxx'
+        },
+        {
+          reg: '^#?(古|诗|词)(名句)?',
+          fnc: 'poetry'
+        }
+      ]
+    })
+  }
 
-    // 金山词霸每日一句
-    // API 文档：https://open.iciba.com/index.php?c=wiki
-    async dsapi(e) {
-        let result = await get_data('https://open.iciba.com/dsapi/')
-        result = await result.json()
-        await e.reply(uploadRecord(result.tts,0,false)) // 发送英语音频，不需要可删除或注释
-        await e.reply(segment.image(result.fenxiang_img))
-        return true
-    }
+  // 金山词霸每日一句
+  // API 文档：https://open.iciba.com/index.php?c=wiki
+  async dsapi(e) {
+    let result = await get_data('https://open.iciba.com/dsapi/')
+    result = await result.json()
+    await e.reply(uploadRecord(result.tts, 0, false)) // 发送英语音频，不需要可删除或注释
+    await e.reply(segment.image(result.fenxiang_img))
+    return true
+  }
 
-    // 一言
-    async hitokoto(e) {
-        let result = await get_data('https://v1.hitokoto.cn/?encode=text&charset=utf-8&c=d&c=i&c=h&c=e')
-        result = await result.text()
-        e.reply(result)
-    }
+  // 一言
+  async hitokoto(e) {
+    let result = await get_data('https://v1.hitokoto.cn/?encode=text&charset=utf-8&c=d&c=i&c=h&c=e')
+    result = await result.text()
+    e.reply(result)
+  }
 
-    // 每日60秒新闻，数据来自《每天60秒读懂世界》公众号
-    async news(e) {
-        e.reply(segment.image('https://api.jun.la/60s.php?format=image'))
-        return true
-    }
+  // 每日60秒新闻，数据来自《每天60秒读懂世界》公众号
+  async news(e) {
+    e.reply(segment.image('https://api.jun.la/60s.php?format=image'))
+    return true
+  }
 
-    // 青年大学习完成图
-    async qndxx(e) {
-      let result = await get_data('https://quickso.cn/api/qndxx/api.php?sort=random')
-      result = await result.text()
-      result = result.replace(/<\/br>/g, '').trim()
-      e.reply(segment.image(`https://${result}`))
+  // 青年大学习完成图
+  async qndxx(e) {
+    let result = await get_data('https://quickso.cn/api/qndxx/api.php?sort=random')
+    result = await result.text()
+    result = result.replace(/<\/br>/g, '').trim()
+    e.reply(segment.image(`https://${result}`))
+  }
+
+  //古诗词名句
+  async poetry(e) {
+    let result = await get_data('https://oiapi.net/API/Sentences')
+    result = await result.json()
+    e.reply(`${result.data.content}——${result.data.author}《${result.data.works}》`)
   }
 }
